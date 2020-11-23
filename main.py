958564ee70432ef7a3d717d573a9a4656d1c93d7
Cory Robbins <coryreedrobbins@live.com>
2020-11-23 18:51:35 +0100

    changed to main.py Flask Rest API with SQLAlchemy and Marshmallow

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+from flask import Flask, request, jsonify
+
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/', methods =['GET'])
+def get_post():
+    return jsonify({"Hello":"World"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
